a112a51132376c474ae1a48b2d773824c6ca9863
Alexssander Leal Luz <alexssander.leal@hotmail.com>
2018-10-29 12:00:50 -0300

    Update MyFirebaseInstanceIDService.java

--- src/android/MyFirebaseInstanceIDService.java
+++ src/android/MyFirebaseInstanceIDService.java
@@ -1,7 +1,7 @@
 package com.gae.scaffolder.plugin;
 
 import android.util.Log;
-import android.FCMPlugin;
+// import android.FCMPlugin;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
